022afb516279f89e7546a1e1ec4567ceb2d2bc27
unknown <Tanga-Johnathan>
2024-03-13 01:25:18 +0300

    looking at collections

---
+++ src/collections/ArrayListss.java
@@ -0,0 +1,24 @@
+package collections;
+import java.util.*;
+
+public class ArrayListss {
+    public static void main(String[] args) {
+        
+        List<Integer> arrayList = new ArrayList<>(5); 
+    
+        //populate my arrayList
+        for(int i=0;i<=5;i++){
+            arrayList.add(i);
+        }
+        System.out.println(arrayList);
+        //remove an item
+        arrayList.remove(2);
+
+        System.out.println(arrayList);
+
+        for(int t=0; t<arrayList.size(); t++){
+            System.out.print(arrayList.get(t));
+        }
+    }
+    
+}
